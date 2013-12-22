e747132d54f4719252fdf548dd5858e16be898c7
Robin Drexler <drexler.robin@gmail.com>
2013-12-22 12:37:09 +0100

    implement first version

--- js/content_script.js
+++ js/content_script.js
@@ -1,3 +1,20 @@
-$(function() {
-    alert('Yay, I\'m an extension :)');
-});
+(function(window) {
+
+	//window parameter, because we might want to workaround some iframe magic sometime
+	function getInput(window) {
+		var searchField = window.document.querySelector('input[type="search"]');
+		var textField = window.document.querySelector('input[type="text"]');
+
+		return searchField || textField	
+	}
+
+	window.addEventListener('keyup', function(e) {
+		var input;
+		var KEY_CODE_FORWARD_SLASH = 191;
+
+		if (e.which === KEY_CODE_FORWARD_SLASH) {
+			input = getInput(window);
+			input && input.focus();
+		}
+	});
+})(window);
