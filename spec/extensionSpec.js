73fdee67e5c4877919c25d503831e2f86c39f506
Robin Drexler <drexler.robin@gmail.com>
2016-07-12 21:42:08 +0200

    Add tests for more cases

--- spec/extensionSpec.js
+++ spec/extensionSpec.js
@@ -6,7 +6,8 @@ var expect = require('chai').expect;
 
 
 describe('extension', function () {
-    it('focusses input on /', function (done) {
+    it('focusses input on / and allows to type', function (done) {
+        var inputField;
         var driver = this.driver();
         driver.get('http://0.0.0.0:3531/simple_input.html');
         driver.findElement(By.css('body'))
@@ -14,10 +15,24 @@ describe('extension', function () {
                 body.sendKeys('/');
             })
             .then(function () {
-                return driver.executeScript('return document.activeElement.id;')
+                return driver.executeScript('return document.activeElement;')
+                    .then(function(input) {
+                        inputField = input;
+                        return inputField.getAttribute('id');
+                    })
             })
             .then(function (inputId) {
+                // make sure input is focussed
                 expect(inputId).to.equal('search');
+            })
+            .then(function () {
+                return inputField.sendKeys('Hello')
+                    .then(function () {
+                        return inputField.getAttribute('value');
+                    })
+            })
+            .then(function (inputValue) {
+                expect(inputValue).to.equal('Hello');
                 done();
             });
 
@@ -51,6 +66,26 @@ describe('extension', function () {
             })
             .then(function (inputValue) {
                 expect(inputValue).to.equal('/');
+                done();
+            });
+    });
+
+    it('does not prevent typing anything else if field was autofocussed by page already', function (done) {
+        var driver = this.driver();
+        driver
+            .get('http://0.0.0.0:3531/autofocus_input.html')
+            .then(function (input) {
+                return driver.executeScript('return document.activeElement;')
+                    .then(function (input) {
+                        return input.sendKeys('T')
+                            .then(function () {
+                                return input.getAttribute('value');
+                            })
+                    })
+
+            })
+            .then(function (inputValue) {
+                expect(inputValue).to.equal('T');
                 done();
             });
 
